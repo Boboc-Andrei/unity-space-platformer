f2c63b9daf43483dc65135a4fad5b567cd3fe40a
Boboc-Andrei <boboc.andrei999@yahoo.com>
2025-05-21 16:18:15 +0300

    Added ICharacterInput interface

---
+++ Assets/Scripts/Characters/Player/ICharacterInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ICharacterInput {
+    public float HorizontalMovement { get; set; }
+    public float VerticalMovement { get; set; }
+    public bool Jump { get; set; }
+    public bool CancelJump { get; set; }
+    public bool Grab { get; set; }
+    public bool HoldJump { get; set; }
+}
