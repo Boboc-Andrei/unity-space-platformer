3d64e13f099e35c8ce49b1f003d2d28547389b41
Boboc-Andrei <boboc.andrei999@yahoo.com>
2025-05-21 17:56:37 +0300

    Added character movement parameters SO

---
+++ Assets/Scripts/ScriptableObjectScripts/CharacterMovementParams.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterMovementParams", menuName = "Scriptable Objects/CharacterMovementParams")]
+public class CharacterMovementParams : ScriptableObject {
+    [Header("Horizontal Movement")]
+    public float TopSpeedX = 9f;
+    public float AccelerationX = .75f;
+
+    [Header("Jumping")]
+    public float JumpSpeed = 21f;
+    [Range(0,1)] public float JumpCutoffFactor = 0.5f;
+    [Range(0, 0.5f)] public float CoyoteTime = 0.15f;
+
+    [Header("Environment")]
+    public float RisingGravity = 4f;
+    public float FallingGravity = 6f;
+    public float TerminalVelocity = 30f;
+    [Range(0, 1)] public float GroundHorizontalDrag = .8f;
+    [Range(0, 1)] public float AirHorizontalDrag = .8f;
+
+    [Header("Wall Sliding")]
+    public float WallSlideAcceleration = 1f;
+    public float WallSlideMaximumVelocity = 2f;
+}
