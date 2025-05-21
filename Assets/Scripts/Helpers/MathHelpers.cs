3ec3ac154358b2ac572a95c978c4de7e50e4bfb8
Boboc-Andrei <boboc.andrei999@yahoo.com>
2025-05-21 16:10:44 +0300

    Create MathHelpers.cs

---
+++ Assets/Scripts/Helpers/MathHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+internal static class Helpers {
+
+    public static float Map(float value, float minIn, float maxIn, float minOut, float maxOut, bool clamp = false) {
+        float val = minOut + (maxOut - minOut) * ((value - minIn) / (maxIn - minIn));
+
+        return clamp ? Mathf.Clamp(val, Mathf.Min(minOut, maxOut), Mathf.Max(minOut, maxOut)) : val;
+    }
+}
